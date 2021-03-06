db2fa7748aa414bac385a13883a0a1cf0752a8de
Thomas Bui <thomas.bui@umontreal.ca>
2021-03-06 18:03:21 -0500

    modifications proposées et réponses aux commentaires du fichier ParcInfo.sql

--- ParcInfo.sql
+++ ParcInfo.sql
@@ -4,40 +4,40 @@
 
 BEGIN transaction;
 
-DROP SCHEMA IF EXISTS ParcInfo CASCADE; -- est-ce nécessaire
+DROP SCHEMA IF EXISTS ParcInfo CASCADE; -- est-ce nécessaire --- utile si jamais on veut faire des modifications. 
 CREATE SCHEMA ParcInfo;
 SET search_path to ParcInfo;
 
-CREATE DOMAIN dom_ad as integer -- ad est une chaîne de caractère ????!!!!
-	check (value >= 0 and value <= 255);
+CREATE DOMAIN dom_ad as varchar(20) -- ad est une chaîne de caractère ????!!!! --- oui parce que sinon '01' va être cast en 1 par exemple
+	check (value >= '0' and value <= '255');
 
 CREATE DOMAIN dom_prix as real
 	check (value >= 0);
 
 CREATE TABLE Segment (
-	indIP integer, -- (exemple:130.120.80)
+	indIP integer primary key, -- (exemple:130.120.80)  --- ajout de la clé primaire
 	nomSegment varchar(20) not null,-- varchar varie alors que char est statique
 	etage integer
 );
 
 CREATE TABLE Salle (
-	nSalle integer,
+	nSalle integer primary key, --- ajout de la clé primaire
 	nomSalle varchar(20) not null,
 	nbPoste integer,
 	indIP integer
 );
 
 CREATE TABLE Poste (
-	nPoste integer,
+	nPoste integer primary key, --- ajout de la clé primaire
 	nomPoste varchar(20) not null,
 	indIP integer,
-	ad dom_ad not null, --dernier groupe de chiffres IP(exemple:11)
+	ad dom_ad, --dernier groupe de chiffres IP(exemple:11) --- pourquoi not null?
 	typePoste varchar(4), --(UNIX,TX,PCWS,PCNT)
 	nSalle integer
 );
 
 CREATE TABLE Logiciel (
-	nLog integer,
+	nLog integer primary key, --- ajout de la clé primaire
 	nomLog varchar(20),
 	dateAch date,
 	version varchar(20),
@@ -46,15 +46,15 @@ CREATE TABLE Logiciel (
 );
 
 CREATE TABLE Installer (
-	numIns serial,
+	numIns serial primary key, --- ajout de la clé primaire
 	nPoste integer,
 	nLog integer,
-	dateIns date not null DEFAULT CURRENT_DATE,
+	dateIns timestamp not null DEFAULT CURRENT_TIMESTAMP, --- dateIns est un Timestamp
 	delai integer
 );
 
 CREATE TABLE Types (
-	typeLP varchar(4),
+	typeLP varchar(4) primary key, --- ajout de la clé primaire
 	nomType varchar(20)
 );
 
